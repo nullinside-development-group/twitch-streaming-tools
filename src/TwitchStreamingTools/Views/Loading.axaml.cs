69ce41218737e8c15d75c92c0975772306ddf2c2
J█████ <dev.nullinside@gmail.com>
2025-06-12 20:51:45 -0400

    chore: removing dead code

--- src/TwitchStreamingTools/Views/Loading.axaml.cs
+++ src/TwitchStreamingTools/Views/Loading.axaml.cs
@@ -8,23 +8,9 @@ namespace TwitchStreamingTools.Views;
 /// </summary>
 public partial class Loading : UserControl {
   /// <summary>
-  ///   The width of the icon.
-  /// </summary>
-  public static readonly StyledProperty<int> WidthProperty =
-    AvaloniaProperty.Register<Loading, int>(nameof(Width), 1);
-
-  /// <summary>
   ///   Initializes a new instance of the <see cref="Loading" /> class.
   /// </summary>
   public Loading() {
     InitializeComponent();
-  }
-
-  /// <summary>
-  ///   The width of the icon.
-  /// </summary>
-  public int Width {
-    get => GetValue(WidthProperty);
-    set => SetValue(WidthProperty, value);
   }
 }
